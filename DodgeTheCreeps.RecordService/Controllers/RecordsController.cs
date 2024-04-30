0e622112f3a7d6b068a708dff725dd5b945856d9
Гладков Егор <gladkovyegor@gmail.com>
2024-04-30 23:05:53 +0400

    some refactoring

--- DodgeTheCreeps.RecordService/Controllers/RecordsController.cs
+++ DodgeTheCreeps.RecordService/Controllers/RecordsController.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -9,24 +8,52 @@ namespace DodgeTheCreeps.RecordService.Controllers;
 [Route("[controller]")]
 public class RecordsController : ControllerBase
 {
-    private readonly ILogger<RecordsController> _logger;
+  private readonly ILogger<RecordsController> _logger;
 
-    public RecordsController(ILogger<RecordsController> logger)
-    {
-        _logger = logger;
-    }
+  public RecordsController(ILogger<RecordsController> logger)
+  {
+    _logger = logger;
+  }
 
-    [HttpGet(Name = "GetUserRecords")]
-    public List<UserRecord> GetUserRecords()
-    {
-        using var dbContext = new UserRecordsDbContext();
-        return dbContext.UserRecords.ToList();
-    }
-    
-    [HttpPost(Name = "SetOrUpdateUserRecord")]
-    public void SetOrUpdateUserRecord(string userName, int record)
-    {
-        using var dbContext = new UserRecordsDbContext();
-        dbContext.AddOrUpdateUserRecord(new UserRecord(userName, record));
-    }
+  [HttpGet(nameof(GetTopRecords))]
+  public IActionResult GetTopRecords()
+  {
+    using var dbContext = new UserRecordsDbContext();
+    return Ok(dbContext.UserRecords.ToList());
+  }
+  
+  [HttpGet(nameof(GetIsUserExists))]
+  public IActionResult GetIsUserExists(string name)
+  {
+    using var dbContext = new UserRecordsDbContext();
+    var user = dbContext.UserRecords.FirstOrDefault(u => u.Username == name);
+    return Ok(user != null);
+  }
+  
+  [HttpPost(nameof(UpdateUserRecord))]
+  public void UpdateUserRecord([FromBody] UserRecord userRecord)
+  {
+    using var dbContext = new UserRecordsDbContext();
+
+    var foundStudent = dbContext.UserRecords.SingleOrDefault(r => r.Username == userRecord.Username);
+    if (foundStudent != null && foundStudent.RecordValue < userRecord.RecordValue)
+      foundStudent.RecordValue = userRecord.RecordValue;
+
+    dbContext.SaveChanges();
+  }
+
+  [HttpPost(nameof(AddUserRecord))]
+  public void AddUserRecord([FromBody] UserRecord userRecord)
+  {
+    using var dbContext = new UserRecordsDbContext();
+    dbContext.UserRecords.Add(new UserRecord(userRecord.Username, userRecord.RecordValue));
+    dbContext.SaveChanges();
+  }
+  
+  [HttpGet(nameof(GetUserRecord))]
+  public IActionResult GetUserRecord(string name)
+  {
+    using var dbContext = new UserRecordsDbContext();
+    return Ok(dbContext.UserRecords.First(u => u.Username == name));
+  }
 }
--- DodgeTheCreeps.RecordService/UserRecordsDbContext.cs
+++ DodgeTheCreeps.RecordService/UserRecordsDbContext.cs
@@ -1,27 +1,10 @@
-﻿using System.Linq;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace DodgeTheCreeps.RecordService;
 
 public class UserRecordsDbContext : DbContext
 {
     public DbSet<UserRecord> UserRecords => Set<UserRecord>();
-
-    public void AddOrUpdateUserRecord(UserRecord userRecord)
-    {
-        if (UserRecords.Contains(userRecord))
-        {
-            var foundStudent = UserRecords.SingleOrDefault(r => r.Username == userRecord.Username);
-            if (foundStudent != null && foundStudent.RecordValue < userRecord.RecordValue)
-                foundStudent.RecordValue = userRecord.RecordValue;
-        }
-        else
-        {
-            UserRecords.Add(userRecord);
-        }
-            
-        SaveChanges();
-    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
